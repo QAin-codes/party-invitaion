bd667c60d5dddc3ccd75b4838d954ed80a578c42
Quratul Ain <107280962+QAin-codes@users.noreply.github.com>
2022-08-03 20:10:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Quratul Ain
+#july 2022
+#party invitation
+
+num= int(input("how many friends do you want to invite to the the party? "))
+if num <10:
+  for i in range(0,num):
+    name=input("enter a name :  ")
+    print(name,"has been invited")
+else:
+  print("too many people")
+
+
